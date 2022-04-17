d38e61af3a2815f328796b663a0b197b85bbbdf1
Pavel Koyushev <pkoyushev@gmail.com>
2022-04-17 11:49:01 +0300

    preparation MainViewController

--- UIKit-Helper/Modules/MainViewController.swift
+++ UIKit-Helper/Modules/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  UIKit-Helper
 //
 //  Created by Pavel Koyushev on 17.04.2022.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
     }
-
-
+    
 }
-
