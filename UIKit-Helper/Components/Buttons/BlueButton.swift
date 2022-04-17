6d2aeac6ea20c41fbc4fea3bede2d06d4bb901a1
Pavel Koyushev <pkoyushev@gmail.com>
2022-04-17 13:20:26 +0300

    added BlueButton and gitignore

---
+++ UIKit-Helper/Components/Buttons/BlueButton.swift
@@ -0,0 +1,36 @@
+//
+//  BlueButton.swift
+//  UIKit-Helper
+//
+//  Created by Pavel Koyushev on 17.04.2022.
+//
+
+import UIKit
+
+class BlueButton: UIButton {
+
+    private let cornerRadius: CGFloat = 10.0
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+
+}
+
+private extension BlueButton {
+    
+    func configure() {
+        backgroundColor = .blue
+        setTitleColor(.white, for: .normal)
+        
+        titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        
+        layer.cornerRadius = cornerRadius
+    }
+}
