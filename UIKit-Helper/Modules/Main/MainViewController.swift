5786230ed5db8211f40f6130b8a9dd56b106b7c7
Pavel Koyushev <pkoyushev@gmail.com>
2022-04-26 19:34:42 +0300

    added button

--- UIKit-Helper/Modules/Main/MainViewController.swift
+++ UIKit-Helper/Modules/Main/MainViewController.swift
@@ -11,6 +11,8 @@ import SnapKit
 class MainViewController: UIViewController {
     
     var oneButton = BlueButton()
+    
+    var twoButton = BlueButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,13 +35,22 @@ extension MainViewController {
         oneButton.setTitle("Diffable Data Source", for: .normal)
         oneButton.addTarget(self, action: #selector(routeToDDSView), for: .touchUpInside)
         
+        twoButton.setTitle("CollectionView Compositional Layout", for: .normal)
+        
         view.addSubview(oneButton)
+        view.addSubview(twoButton)
     }
     
     func makeConstraints() {
         
         oneButton.snp.makeConstraints {
             $0.center.equalToSuperview()
+            $0.height.equalTo(40)
+            $0.horizontalEdges.equalToSuperview().inset(45)
+        }
+        
+        twoButton.snp.makeConstraints {
+            $0.top.equalTo(oneButton.snp.bottom).offset(20)
             $0.height.equalTo(40)
             $0.horizontalEdges.equalToSuperview().inset(45)
         }
